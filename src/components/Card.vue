2b5e1cb48365ec5e03fa65bc3d1d1ae00b4237a4
0xlarry <26318510+0xlarry@users.noreply.github.com>
2021-03-22 22:07:53 -0500

    Card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,98 @@
 <template>
-  <div></div>
+  <div class="col-4 ps-2 pe-2 mb-3">
+    <div class="card d-flex flex-row align-items-center">
+      <div v-if="iconType == 'svg'" class="card-icon">
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 512 512"
+          class="icon undefined"
+          height="1.05em"
+          width="1.05em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path :d="iconData"></path>
+        </svg>
+      </div>
+      <div v-if="iconType == 'png'" class="image-cropper">
+        <img :src="iconData" />
+      </div>
+      <span class="me-auto">{{ name }}</span>
+      <span class="fs-5 ms-auto">{{ formatMoney(value) }}</span>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, PropType } from "vue";
 
 export default defineComponent({
   name: "Card",
+  props: {
+    name: String,
+    value: Number,
+    iconType: String as PropType<"svg" | "png">,
+    iconData: String,
+  },
+  methods: {
+    formatMoney(value: number, currencySymbol = "$"): string {
+      // If the number is zero, then simply return `$0`
+      if (value == 0) return currencySymbol + "0";
+
+      let formatted = Math.floor(value)
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+      // If the value is no less than 1, then 2 decimal places
+      // Otherwise, 4 decimal places
+      let decPlaces = value >= 1 ? 2 : 4;
+
+      formatted += "." + value.toFixed(decPlaces).split(".")[1];
+      return currencySymbol + formatted;
+    },
+  },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.card {
+  padding: 1rem;
+  font-weight: 700;
+  transition: all 0.25s;
+}
+
+.card:hover {
+  cursor: pointer;
+  box-shadow: #dfe8f9 0px 0px 0px 3px;
+}
+
+.card-icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  width: 40px;
+  margin-right: 16px;
+  background: rgba(0, 216, 151, 0.15);
+  border-radius: 50%;
+}
+
+.card-icon svg {
+  fill: #00d897;
+}
+
+.image-cropper {
+  width: 40px;
+  height: 40px;
+  position: relative;
+  overflow: hidden;
+  border-radius: 50%;
+  margin-right: 16px;
+}
+
+.image-cropper img {
+  width: 40px;
+  height: 40px;
+}
+</style>
